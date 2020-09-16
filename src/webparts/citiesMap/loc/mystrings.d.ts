67124e69deb139a3ffcdb6bc39e98fce31a3ba47
Enver Selchuk <Enver.Selchuk@gamma.co.uk>
2020-09-16 12:04:45 +0100

    convert csom on cloud version

---
+++ src/webparts/citiesMap/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICitiesMapWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CitiesMapWebPartStrings' {
+  const strings: ICitiesMapWebPartStrings;
+  export = strings;
+}
---
+++ src/webparts/csomDemo/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICsomDemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CsomDemoWebPartStrings' {
+  const strings: ICsomDemoWebPartStrings;
+  export = strings;
+}
